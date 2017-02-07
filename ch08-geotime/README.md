0472e289884a977693a90c157371c27f5842b21a
Felipe <flpeacs@gmail.com>
2017-02-07 15:23:43 -0200

    Adding NYC app

---
+++ ch08-geotime/README.md
@@ -0,0 +1,44 @@
+# Spark applied to NYC taxi data
+
+Code was adapted from the book Advanced Analytics with Spark, by Juliet Hougland, Uri Laserson, Sean Owen, Sandy Ryza, and Josh Wills.
+
+NYC regions that are being used are available at [here](https://github.com/dwillis/nyc-maps/blob/master/city_council.geojson).
+
+Spark Version: 2.1.0 with Hadoop 2.7
+Java Version: 1.8
+
+**src/main/scala/com/cloudera/datascience/geotime/RunGeoTime.scala** calculates the average duration of taxi rides that depart from a given NYC region in a given hour of the day. Code uses dataframes to perform operations.
+
+Running the application:
+
+Download the dataset at [here](http://www.andresmh.com/nyctaxitrips/)
+Unzip the dataset to ```/ch08-geotime/taxidata/```
+```mvn package```
+```/YOUR_SPARK_HOME/bin/spark-submit target/ch08-geotime-2.0.0-jar-with-dependencies.jar``` 
+
+Example of output: 
+
++-------+------+------------------+
+|borough|pickup|        avg       |
++-------+------+------------------+
+|      1|     0|12.826692045758886|
+|      1|     1|12.770052670047395|
+|      1|     2|11.595311437372485|
+|      1|     3|12.954744036779656|
+|      1|     4|12.738297778236447|
+|      1|     5|13.145644095788604|
+|      1|     6|13.222581079262548|
+|      1|    18| 11.70735953927543|
+|      1|    19|12.737486864126415|
+|      1|    20|11.714717060207454|
+|      1|    21|12.910486190734819|
+|      1|    22| 13.02640610019444|
+|      1|    23|12.977898676806342|
+|     10|     0| 18.29158993247391|
+|     10|     1|19.242752293577983|
+|     10|     2|17.234633319203052|
+|     10|     3|18.226008617312964|
+|     10|     4| 18.27751677852349|
+|     10|     5|18.758107507774323|
+|     10|     6|19.057035064358633|
++-------+------+------------------+
---
+++ ch08-geotime/src/main/scala/com/cloudera/datascience/geotime/RunGeoTime.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015 and onwards Sanford Ryza, Juliet Hougland, Uri Laserson, Sean Owen and Joshua Wills
+ *
+ * See LICENSE file for further information.
+ */
+
+package com.cloudera.datascience.geotime
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.functions._
+
+import com.esri.core.geometry.Point
+import spray.json._
+
+import com.cloudera.datascience.geotime.GeoJsonProtocol._
+
+class RichRow(row: Row) {
+  def getAs[T](field: String): Option[T] =
+    if (row.isNullAt(row.fieldIndex(field))) None else Some(row.getAs[T](field))
+}
+
+case class Trip(
+  license: String,
+  pickupTime: Long,
+  dropoffTime: Long,
+  pickupX: Double,
+  pickupY: Double,
+  dropoffX: Double,
+  dropoffY: Double)
+
+object RunGeoTime extends Serializable {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+    val taxiRaw = spark.read.option("header", "true").csv("taxidata")
+    val taxiParsed = taxiRaw.rdd.map(safe(parse))
+    val taxiGood = taxiParsed.map(_.left.get).toDS
+    taxiGood.cache()	
+
+    val hours = (pickup: Long, dropoff: Long) => {
+      TimeUnit.HOURS.convert(dropoff - pickup, TimeUnit.MILLISECONDS)
+    }
+
+    val hoursUDF = udf(hours)
+    taxiGood.groupBy(hoursUDF($"pickupTime", $"dropoffTime").as("h")).count().sort("h")
+
+    spark.udf.register("hours", hours)
+    val taxiClean = taxiGood.where("hours(pickupTime, dropoffTime) >= 0")
+
+    val geojson = scala.io.Source.fromURL(this.getClass.getResource("/nyc-city_council.geojson")).mkString
+
+    val bFeatures = spark.sparkContext.broadcast(geojson.parseJson.convertTo[FeatureCollection])
+  
+    val bLookup = (x: Double, y: Double) => {
+      val feature: Option[Feature] = bFeatures.value.find(f => {
+        f.geometry.contains(new Point(x, y))
+      })
+      feature.map(f => {
+        f("CounDist").convertTo[String]
+      }).getOrElse("NA")
+    }
+
+    val taxiDone = taxiClean.where("dropoffX != 0 and dropoffY != 0 and pickupX != 0 and pickupY != 0")
+    taxiGood.unpersist()
+	    
+    val boroughDurations = taxiDone.map(p => (bLookup(p.dropoffX, p.dropoffY), (p.pickupTime % 86400) / 3600, p.dropoffTime - p.pickupTime)).toDF("borough", "pickup", "count")
+
+    boroughDurations.orderBy("borough", "pickup").groupBy("borough", "pickup").agg(avg("count")).show()
+	
+    boroughDurations.unpersist()
+  }
+
+  def safe[S, T](f: S => T): S => Either[T, (S, Exception)] = {
+    new Function[S, Either[T, (S, Exception)]] with Serializable {
+      def apply(s: S): Either[T, (S, Exception)] = {
+        try {
+          Left(f(s))
+        } catch {
+          case e: Exception => Right((s, e))
+        }
+      }
+    }
+  }
+
+  def parseTaxiTime(rr: RichRow, timeField: String): Long = {
+    val formatter = new SimpleDateFormat(
+       "yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+    val optDt = rr.getAs[String](timeField)
+    optDt.map(dt => formatter.parse(dt).getTime / 60000).getOrElse(0L)
+  }
+
+  def parseTaxiLoc(rr: RichRow, locField: String): Double = {
+    rr.getAs[String](locField).map(_.toDouble).getOrElse(0.0)
+  }
+
+  def parse(line: Row): Trip = {
+    val rr = new RichRow(line)
+    Trip(
+      license = rr.getAs[String]("hack_license").orNull,
+      pickupTime = parseTaxiTime(rr, "pickup_datetime"),
+      dropoffTime = parseTaxiTime(rr, "dropoff_datetime"),
+      pickupX = parseTaxiLoc(rr, "pickup_longitude"),
+      pickupY = parseTaxiLoc(rr, "pickup_latitude"),
+      dropoffX = parseTaxiLoc(rr, "dropoff_longitude"),
+      dropoffY = parseTaxiLoc(rr, "dropoff_latitude")
+    )
+  }
+}
